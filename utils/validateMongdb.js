bea31308f8250e3ee004ea55b834b8e77a5e108c
long1703@outlook.com <long1703@outlook.com>
2023-03-22 00:16:07 +0700

    update

--- utils/validateMongdb.js
+++ utils/validateMongdb.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const validateMongodb = (id) => {
-    const isValid = mongoose1.Types.ObjectId.isValid(id);
+    const isValid = mongoose.Types.ObjectId.isValid(id);
     if (!isValid) throw new Error("This is not valid or not found")
 };
 
